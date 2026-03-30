0c5862f96a6b83a997d93f19f30f09c60f6f48ac
swufan <2915475627@qq.com>
2026-03-31 05:27:00 +0800

    feat: init Maven project with Spring Boot 3.2

---
+++ src/main/java/com/powerflow/workflow/PowerflowApplication.java
@@ -0,0 +1,11 @@
+package com.powerflow.workflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerflowApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PowerflowApplication.class, args);
+    }
+}
