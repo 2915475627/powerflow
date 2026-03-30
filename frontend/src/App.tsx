ad5bfd2fc0e6a9195ae7d76984b3c396a8ec242a
swufan <2915475627@qq.com>
2026-03-31 07:39:40 +0800

    feat(subproject-3): init React + Vite + TypeScript frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import { Routes, Route } from 'react-router-dom'
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<div>PowerFlow Admin</div>} />
+    </Routes>
+  )
+}
+
+export default App
